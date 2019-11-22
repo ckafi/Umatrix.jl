59a6fe2f4d45a18fdd59d35a17f15b85c85b5f22
Tobias Frilling <tobias@frilling-online.de>
2019-11-22 15:45:02 +0100

    Start top-down development

---
+++ src/Settings.jl
@@ -0,0 +1,43 @@
+# Copyright 2019 Tobias Frilling
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Base.@kwdef mutable struct Settings
+    rows::Int                     = 50
+    columns::Int                  = 82
+    toroid::Bool                  = true
+    epochs::Int                   = 24
+    startLearningRate::Float64    = 0.5
+    endLearningRate::Float64      = 0.1
+    startRadius::Int              = 24
+    endRadius::Int                = 1
+    init_method::Symbol           = :uniform_min_max
+    neighbourhoodFunction::Symbol = :gauss
+    neighbourhoodCooling::Symbol  = :linear
+    learningRateCooling::Symbol   = :linear
+end
+
+const defaultSettings = Settings()
+
+macro unpack(exp)
+    keys = isa(exp, Symbol) ? [exp] : exp.args
+    assigments = [:( $key = getproperty(settings, $(Expr(:quote, key))) ) for key in keys]
+    esc(Expr(:block, assigments...))
+end
+
+function Base.show(io::IO, ::MIME"text/plain", settings::Settings)
+    print(io, "Umatrix settings:")
+    for name in fieldnames(Settings)
+        print(io, "\n$(name) = ", getproperty(settings, name))
+    end
+end
--- src/Umatrix.jl
+++ src/Umatrix.jl
@@ -14,9 +14,28 @@
 
 module Umatrix
 
-using Distributions
+macro todo()
+    :(error("not implemented"))
+end
 
-function esomInit(data::Matrix{Float64}; init_method = :uniform_min_max, rows = 50, columns = 82)
+using Distances
+using Distributions
+using Random
+
+include("Settings.jl")
+
+function bestMatch(dataPoint::Vector{<:Real}, weights::Matrix{<:Real})
+    @assert size(dataPoint, 1) == size(weights, 2)
+    dist(v1,v2) = (v1 .- v2).^2 |> sum
+    return map(row -> dist(row, dataPoint), eachrow(weights)) |> argmin
+end
+
+function bestMatches(data::Matrix{<Real}, weights::Matrix{<:Real})
+    @todo
+end
+
+function esomInit(data::Matrix{<:Real}, settings = defaultSettings)
+    @unpack rows, columns, init_method
     randcol = if init_method == :uniform_min_max
                   col -> rand(Uniform(minimum(col), maximum(col)), rows * columns)
               elseif init_method == :uniform_mean_std
@@ -29,6 +48,37 @@ function esomInit(data::Matrix{Float64}; init_method = :uniform_min_max, rows = 
                   throw(ArgumentError("$(init_method) is not a valid initialization method"))
               end
     return mapslices(randcol, data, dims = 1)
+end
+
+function esomTrainOnline(data::Matrix{<:Real}, weights::Matrix{<:Real}, settings = defaultSettings)
+    @assert size(data, 2) == size(weights, 2)
+    @todo
+end
+
+function pmatrixForEsom(args...)
+    @todo
+end
+
+function shiftedNeurons(args...)
+    @todo
+end
+
+function esomTrain(data::Matrix{<:Real}, key = 1:size(data,1), settings = defaultSettings;
+                   shiftToHighestDensity = false)
+    @todo
+    @assert size(data, 2) == size(key, 1)
+    grid = esomInit(data, settings)
+    weights = esomTrainOnline(data, grid, settings)
+    if shiftToHighestDensity && settings.toroid
+        println("Shift to point of highest density")
+        radius = filter(!iszero, pairwise(Euclidean(), data, dims=1)) |> mean
+        pmatrix = pmatrixForEsom(data, weights, radius, settings)
+        pos = findall(isequal(maximum(pmatrix)), pmatrix)[1]
+        weights = shiftedNeurons(Weights, Lines, Columns, -pos[1], -pos[2])
+    end
+    projection = bestMatches(data, weights, settings.columns)
+    umatrix = umatrixForEsom(weights, settings)
+    return projection, weights, umatrix
 end
 
 end # module
