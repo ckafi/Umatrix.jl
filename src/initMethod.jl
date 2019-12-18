d061e6b6eb7e8145a0a35123da3efe74085f6f46
Tobias Frilling <tobias@frilling-online.de>
2019-12-18 16:01:27 +0100

    Add docstrings for initMethods

--- src/initMethod.jl
+++ src/initMethod.jl
@@ -15,18 +15,43 @@
 @inline initMethod(settings::Settings = defaultSettings) =
     initMethod(Val(settings.initMethod), settings)
 
+"""
+    initMethod(::Val{:uniform_min_max})
+
+Returns an initializer function. The values are uniformly distributed over
+``[\\min(c), \\max(c)]`` of the data column.
+"""
 function initMethod(::Val{:uniform_min_max}, settings::Settings = defaultSettings)
     col -> rand(Uniform(minimum(col), maximum(col)), prod(settings.latticeSize))
 end
 
+"""
+    initMethod(::Val{:uniform_mean_std})
+
+Returns an initializer function. The values are uniformly distributed between
+``[μ-2σ,μ+2σ]`` of the data column.
+"""
 function initMethod(::Val{:uniform_mean_std}, settings::Settings = defaultSettings)
-    col -> rand(Uniform(mean(col), std(col)), prod(settings.latticeSize))
+    m = mean(col)
+    s = std(col)
+    col -> rand(Uniform(m - 2s, m + 2s), prod(settings.latticeSize))
 end
 
+"""
+    initMethod(::Val{:normal_mean_std})
+
+Returns an initializer function. The values are Normal distributed with the same
+mean and std as the data column.
+"""
 function initMethod(::Val{:normal_mean_std}, settings::Settings = defaultSettings)
-    col -> rand(Uniform(mean(col), std(col)), prod(settings.latticeSize))
+    col -> rand(Normal(mean(col), std(col)), prod(settings.latticeSize))
 end
 
+"""
+    initMethod(::Val{:zeros})
+
+Returns an initializer function. The values are set to 0.
+"""
 function initMethod(::Val{:zeros}, settings::Settings = defaultSettings)
     col -> zeros(prod(settings.latticeSize))
 end
